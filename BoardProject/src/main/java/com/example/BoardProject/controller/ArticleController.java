e5aebef4d96806be2faa0c75e3f0c4ce4e0fece1
BuildEnough <rhtmaehcl878@naver.com>
2023-11-24 16:21:49 +0900

    회원가입 리팩터링: controller, dto, entity

--- BoardProject/src/main/java/com/example/BoardProject/controller/ArticleController.java
+++ BoardProject/src/main/java/com/example/BoardProject/controller/ArticleController.java
@@ -22,15 +22,12 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
-        // System.out.println(form.toString());
         log.info(form.toString());
 
         Article article = form.toEntity();
-        // System.out.println(article.toString());
         log.info(article.toString());
 
         Article saved = articleRepository.save(article);
-        // System.out.println(saved.toString());
         log.info(saved.toString());
         return "";
     }
