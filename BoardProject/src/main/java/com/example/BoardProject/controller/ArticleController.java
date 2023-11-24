1d42d835e18d15abba00dba4c6cf9c0dc96f69cc
BuildEnough <rhtmaehcl878@naver.com>
2023-11-24 15:14:13 +0900

    dto를 DB에 저장

--- BoardProject/src/main/java/com/example/BoardProject/controller/ArticleController.java
+++ BoardProject/src/main/java/com/example/BoardProject/controller/ArticleController.java
@@ -1,12 +1,17 @@
 package com.example.BoardProject.controller;
 
 import com.example.BoardProject.dto.ArticleForm;
+import com.example.BoardProject.entity.Article;
+import com.example.BoardProject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+    @Autowired
+    private ArticleRepository articleRepository;
 
     @GetMapping("/articles/new")
     public String newArticleForm() {
@@ -16,6 +21,12 @@ public class ArticleController {
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
         System.out.println(form.toString());
+
+        Article article = form.toEntity();
+        System.out.println(article.toString());
+
+        Article saved = articleRepository.save(article);
+        System.out.println(saved.toString());
         return "";
     }
 }
