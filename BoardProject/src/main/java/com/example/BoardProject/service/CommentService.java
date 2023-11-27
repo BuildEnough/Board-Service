f40f52758524fb441a2d57cc957729559466f82d
BuildEnough <rhtmaehcl878@naver.com>
2023-11-27 14:47:45 +0900

    댓글 조회 요청

---
+++ BoardProject/src/main/java/com/example/BoardProject/service/CommentService.java
@@ -0,0 +1,33 @@
+package com.example.BoardProject.service;
+
+import com.example.BoardProject.dto.CommentDto;
+import com.example.BoardProject.entity.Comment;
+import com.example.BoardProject.repository.ArticleRepository;
+import com.example.BoardProject.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CommentService {
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    public List<CommentDto> comments(Long articleId) {
+        List<Comment> comments = commentRepository.findByArticleId(articleId);
+
+        List<CommentDto> dtos = new ArrayList<CommentDto>();
+        for (int i = 0; i < comments.size(); i++) {
+            Comment c = comments.get(i);
+            CommentDto dto = CommentDto.createCommentDto(c);
+            dtos.add(dto);
+
+        }
+            return dtos;
+    }
+}
